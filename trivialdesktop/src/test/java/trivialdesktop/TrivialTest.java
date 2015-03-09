207b802057416aa9fbac651654a7a5e2d06cca58
rubcuevas <rubencuevas93@gmail.com>
2015-03-09 12:53:35 +0100

    Add TrivialTest

---
+++ trivialdesktop/src/test/java/trivialdesktop/TrivialTest.java
@@ -0,0 +1,14 @@
+package trivialdesktop;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrivialTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

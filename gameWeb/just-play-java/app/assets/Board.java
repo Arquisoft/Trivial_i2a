f7ac4b449078713bc1dcfe9018d8840ea7b7d64e
Adan <adanASW@outlook.com>
2015-04-23 13:52:13 +0200

    Setup Play Framework
    I have configured the play Framework and prepared it in order to support
    REST and HTML, I have also included one example so we can start doing
    our project.

---
+++ gameWeb/just-play-java/app/assets/Board.java
@@ -0,0 +1,10 @@
+package assets;
+
+import java.util.List;
+
+public class Board {
+    public List<Box> boxes;
+    public Player playerTurn;
+    public List<Player> players;
+    public String test;
+}
